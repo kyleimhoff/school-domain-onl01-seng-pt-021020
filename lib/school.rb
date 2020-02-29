65a68d9785d973767b79975beedf80403ecf01f1
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-29 17:07:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -32,7 +32,15 @@ end
   
   
   school = School.new("Bayside High School")
-  school.add_student("AC Slater", 9)
-school.add_student("Kelly Kapowski", 10)
-school.add_student("Screech", 11)
 school.roster
+school.add_student("Zach Morris", 9)
+school.add_student("Moshe Zeus", 9)
+school.add_student("Onkelos Octavius", 9)
+school.roster
+school.add_student("Sugar Zaza", 5)
+school.roster
+school.add_student("Theo Madus", 5)
+school.add_student("Abbah Zabbah", 2)
+school.roster
+school.grade(9)
+school.sort
