0ac60f81472179666daa8df3f8fa7135dd400de4
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-29 16:52:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,6 +9,16 @@ class School
   end
   
   def roster
+    
+  def sort 
+     nu_hash = {}
+    roster.each do |x, y| 
+      nu_hash[x] = y.sort 
+    end 
+    nu_hash
+end 
+  
+end 
   
   
   school = School.new("Bayside High School")
