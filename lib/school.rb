bd3e1f74c0f50646dc58a30b8022c0dac4c7ebf0
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-29 16:53:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,12 @@ class School
     
   end
   
-  def roster
+  def add_student(student, level)
+    # this sort of means #=> a || a = b ... or #=> x || x = y
+    # it's freak'n weird
+    roster[level] ||= []
+    roster[level] << student
+  end
     
   def sort 
      nu_hash = {}
