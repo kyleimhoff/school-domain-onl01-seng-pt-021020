77dc2c51db7abeb08e1a1d28011812097263c699
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-29 17:14:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -20,11 +20,7 @@ class School
     end 
   end  
   def sort 
-     nu_hash = {}
-    roster.each do |x, y| 
-      nu_hash[x] = y.sort 
-    end 
-    nu_hash
+     roster.sort{|k, v| v}
   end 
   
   
